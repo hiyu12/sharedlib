aadd3231b3b482c30cbfa06bded1251131dcaaed
hiyu12 <107801575+hiyu12@users.noreply.github.com>
2022-12-15 18:47:00 +0530

    Update and rename Calculate.groovy to calculate.groovy

--- vars/Calculate.groovy
+++
@@ -1,6 +0,0 @@
-def add(a,b){
-    echo "Sum of ${a} and ${b} is ${a+b}"
-}
-def mul(a,b){
-    echo "Mul of ${a} and ${b} is ${a*b}"
-}
---
+++ vars/calculate.groovy
@@ -0,0 +1,6 @@
+def add(x,y){
+    echo "Sum of ${x} and ${y} is ${x+y}"
+}
+def mul(k,l){
+    echo "Mul of ${k} and ${l} is ${k*l}"
+}
